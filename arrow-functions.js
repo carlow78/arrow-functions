8ec2bf695309862c852d8ba83de4756b540b66f7
John Lynch <johnf.lynch@yahoo.ie>
2024-05-14 19:43:07 +0100

    update

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,24 @@
  */
 
 // Vanilla JavaScript Function
+const addTwoNumbers = (a,b) => {
+    //Code Block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3,5);
+consolelog(sum);
 
 // Arrow Function With Parameters
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6,4);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
 
+const sayHello = () => console.log('hello');
 // Returning Multiple Lines
